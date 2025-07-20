1edc1b3ebf092a5a7c420192fea70fb5a9bc8ddd
Matos52 <Matej.Regec@gmail.com>
2025-07-20 23:08:54 +0200

    Creation of NavItem component

---
+++ components/NavItems.tsx
@@ -0,0 +1,47 @@
+import { cn } from "lib/utils";
+import { href, Link, NavLink } from "react-router";
+import { sidebarItems } from "~/constants";
+
+const NavItems = () => {
+    const user = {
+        name: "Matej",
+        email: "matej@gmail.com",
+        imageUrl: "/assets/images/david.webp"
+    }
+
+
+  return (
+    <section className="nav-items">
+      <Link to="/" className="link-logo">
+        <img src="/assets/icons/logo.svg" alt="logo" />
+        <h1>Tourvisto</h1>
+      </Link>
+      <div className="container">
+        <nav>
+          {sidebarItems.map(({ id, href, icon, label }) => (
+            <NavLink to={href} key={id}>
+              {({ isActive }: { isActive: boolean }) => (
+                <div
+                  className={cn("group nav-item", {
+                    "bg-primary-100 !text-white": isActive,
+                  })}
+                >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-0 group-hover:invert ${
+                      isActive ? "brightness-0 invert" : "text-dark-200"
+                    }`}
+                  />
+                  {label}
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default NavItems;
