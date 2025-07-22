6884d08063face152449cbae9d9810732769ed68
Matos52 <Matej.Regec@gmail.com>
2025-07-22 22:05:14 +0200

    Creation of StatsCard and TripCard component.

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips page"
+        description="Check out our current users in real time"
+      />
+
+      All Users page contents
+    </main>
+  );
 };
 
 export default AllUsers;
