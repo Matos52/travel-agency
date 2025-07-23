137c313f2e4b253176792750a1a04a01a250ea11
Matos52 <Matej.Regec@gmail.com>
2025-07-23 22:47:21 +0200

    StatsCard continue implementation

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -35,7 +35,7 @@ const dashboard = () => {
       />
 
       <section className="flex flex-col gap-6">
-        <div className="grip grid-cols-1 md:grid-cols-3 gap-6 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
           <StatsCard
             headerTitle="Total Users"
             total={totalUsers}
