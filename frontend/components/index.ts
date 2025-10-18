364e2a2e6cb3bcae232181e244a32fe2fc6770f2
Matos52 <Matej.Regec@gmail.com>
2025-10-18 22:34:02 +0200

    First commit

---
+++ frontend/components/index.ts
@@ -0,0 +1,5 @@
+export { default as NavItems } from "./NavItems";
+export { default as MobileSidebar } from "./MobileSiderbar";
+export { default as Header } from "./Header";
+export { default as StatsCard } from "./StatsCard";
+export { default as TripCard } from "./TripCard";
